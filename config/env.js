9878685cee7c0ce63482d86ddaf53fbb27fdff90
Tyler Nappy <tnappy@tnappy-mbp.plaid.com>
2021-01-17 17:52:39 -0500

    added accessToken to env.js

--- config/env.js
+++ config/env.js
@@ -24,6 +24,7 @@ const environments = {
             username: process.env.PARTICLE_USERNAME,
             password: process.env.PARTICLE_PASSWORD,
             deviceId: process.env.PARTICLE_DEVICE_ID,
+            accessToken: process.env.PARTICLE_ACCESS_TOKEN, // generated programmtically but stored here b/c expires ini 90 days https://docs.particle.io/reference/SDKs/javascript/#logging-in
         },
     },
 };
