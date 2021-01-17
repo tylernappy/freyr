eee3b739a4f3ba9c35aea310f3b1491080a17b8e
Tyler Nappy <tnappy@tnappy-mbp.plaid.com>
2021-01-17 13:39:08 -0500

    added / route

--- routes/main-routes.js
+++ routes/main-routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { waterPlant } = require('./lib/particle');
+const path = require('path');
+const { waterPlant } = require('../lib/particle');
 
 module.exports = () => {
+    router.get('/', async (req, res) => {
+        res.sendFile(path.join(process.cwd(), 'views', 'index.html'));
+    });
+
     router.get('/water-plant', async (req, res) => {
         waterPlant();
         res.sendStatus(200);
